bdfbf0f649c58a199b3d9ef1e163c9425ffb199a
TytanRock <coryness@tytan.rocks>
2021-04-27 22:09:57 -0400

    Changed backend to using Spring Boot

---
+++ backend/src/main/java/com/cs471/studentLoanSystem/StudentLoanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cs471.studentLoanSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLoanSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLoanSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cs471/studentLoanSystem/StudentLoanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs471.studentLoanSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLoanSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
