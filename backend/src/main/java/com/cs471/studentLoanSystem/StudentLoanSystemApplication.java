bb6d2e29779ecb80a452d5c13fced7b904d8bb7d
TytanRock <coryness@tytan.rocks>
2021-04-28 09:01:54 -0400

    Added bootRun and test gradle tasks
    Also added rest framework so that we can map URLs to functions.
    Will be creating github action tasks to test the framework

--- backend/src/main/java/com/cs471/studentLoanSystem/StudentLoanSystemApplication.java
+++ backend/src/main/java/com/cs471/studentLoanSystem/StudentLoanSystemApplication.java
@@ -2,12 +2,32 @@ package com.cs471.studentLoanSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentLoanSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentLoanSystemApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World!";
+	}
+
+
+	/**
+	 * close_this_application mapping
+	 * This function is used only to ensure the bootRun gradle task
+	 * finishes. This will be disabled on production runs so that
+	 * nobody can close the web application, but it's nice to close
+	 * the application when doing bootRun tests.
+	 */
+	@RequestMapping("/close_this_application")
+	public void quit() {
+		System.exit(0);
+	}
 }
