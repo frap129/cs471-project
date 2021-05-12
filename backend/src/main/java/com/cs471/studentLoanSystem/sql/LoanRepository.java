cf3238867ec7ee17b230e57c4bf0a29b6244e67f
Joe Maples <joe@maples.dev>
2021-05-12 16:23:43 -0400

    backend: Remove LoanRepo findByID method

--- backend/src/main/java/com/cs471/studentLoanSystem/sql/LoanRepository.java
+++ backend/src/main/java/com/cs471/studentLoanSystem/sql/LoanRepository.java
@@ -4,7 +4,5 @@ import com.cs471.studentLoanSystem.sql.descriptions.Loan;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LoanRepository extends CrudRepository<Loan, Integer> {
-    Loan findByID(Integer id);
-
-    Loan[] findAllByStudentID(Integer studentId);
+    Loan[] findAllByStudentId(Integer studentId);
 }
