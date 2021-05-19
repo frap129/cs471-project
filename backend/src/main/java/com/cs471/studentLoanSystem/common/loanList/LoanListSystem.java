a00cb55d4da3a85affafcff2dd52d283085356dd
Joe Maples <joe@maples.dev>
2021-05-19 14:23:37 -0400

    backend: Add School name filtering to loan list

--- backend/src/main/java/com/cs471/studentLoanSystem/common/loanList/LoanListSystem.java
+++ backend/src/main/java/com/cs471/studentLoanSystem/common/loanList/LoanListSystem.java
@@ -2,9 +2,6 @@ package com.cs471.studentLoanSystem.common.loanList;
 
 import com.cs471.studentLoanSystem.sql.LoanRepository;
 import com.cs471.studentLoanSystem.sql.descriptions.Loan;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Predicate;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 @RestController
 public class LoanListSystem {
@@ -25,23 +26,28 @@ public class LoanListSystem {
         return p -> p.getBankId() != id;
     }
 
-    @PostMapping("/registrar-loan-list")
+    @PostMapping("/loan-list")
     public ResponseEntity<LoanListResponse> loanList(
             @RequestBody LoanListInformation info, @NotNull Model model) {
         model.addAttribute("LoanFilters", info);
 
         // Get the list of loans
         List<Loan> loans = null;
-        if (info.getStudentId() != null) {
+        if (info.getSchoolName() != null) {
+            loans = Arrays.asList(loanRepo.findAllBySchoolName(info.getSchoolName()));
+            if (info.getBankId() != null) {
+                loans.removeIf(wrongBankId(info.getBankId()));
+            }
+            if (info.getStudentId() != null) {
+                loans.removeIf(wrongStudentId(info.getStudentId()));
+            }
+        } else if (info.getStudentId() != null) {
             loans = Arrays.asList(loanRepo.findAllByBankId(info.getStudentId()));
             if (info.getBankId() != null) {
                 loans.removeIf(wrongBankId(info.getBankId()));
             }
         } else if (info.getBankId() != null) {
             loans = Arrays.asList(loanRepo.findAllByBankId(info.getBankId()));
-            if (info.getStudentId() != null) {
-                loans.removeIf(wrongStudentId(info.getStudentId()));
-            }
         }
 
         LoanListResponse response = new LoanListResponse();
--- backend/src/main/java/com/cs471/studentLoanSystem/sql/LoanRepository.java
+++ backend/src/main/java/com/cs471/studentLoanSystem/sql/LoanRepository.java
@@ -1,6 +1,7 @@
 package com.cs471.studentLoanSystem.sql;
 
 import com.cs471.studentLoanSystem.sql.descriptions.Loan;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LoanRepository extends CrudRepository<Loan, Integer> {
@@ -9,4 +10,7 @@ public interface LoanRepository extends CrudRepository<Loan, Integer> {
     Loan[] findAllByStudentId(Integer studentId);
 
     Loan[] findAllByBankId(Integer bankId);
+
+    @Query("SELECT * FROM Loan u JOIN Student s on s.studentId = u.studentId WHERE s.SchoolName = ?1")
+    Loan[] findAllBySchoolName(String schoolName);
 }
