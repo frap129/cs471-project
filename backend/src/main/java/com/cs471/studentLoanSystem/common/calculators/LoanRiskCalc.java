53a8e7170c21e5a44462a77f250cb878a2e7127f
tbolar18 <tbolar2016@gmail.com>
2021-06-13 15:52:30 -0400

    tried reformatting the two calculators

--- backend/src/main/java/com/cs471/studentLoanSystem/common/calculators/LoanRiskCalc.java
+++ backend/src/main/java/com/cs471/studentLoanSystem/common/calculators/LoanRiskCalc.java
@@ -1,15 +1,15 @@
 package com.cs471.studentLoanSystem.common.calculators;
 
 public class LoanRiskCalc {
-    public static double loanRiskCalculator(double income, int score){
+    public static double loanRiskCalculator(double income, int score) {
         double checkIncome = 0;
         int checkScore = 0;
         double risk = 1;
-        while (income > 0){
+        while (income > 0) {
             risk = risk - 0.05;
             income = income - 10000;
         }
-        while(score > 0){
+        while (score > 0) {
             risk = risk - 0.06;
             score = score - 100;
         }
